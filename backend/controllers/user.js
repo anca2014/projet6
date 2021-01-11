b2144161a525914cf608e7d3439f51bc0c6355c2
anca2014 <aureliemichel@orange.fr>
2021-01-11 14:26:55 +0100

    commit final

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,23 +11,19 @@ const User = require('../models/User');
 //masquage e-mail
 const emailMask2Options = {
     maskWith: "*", 
-    unmaskedStartCharactersBeforeAt: 3,
-    unmaskedEndCharactersAfterAt: 2,
+    unmaskedStartCharactersBeforeAt: 5,
+    unmaskedEndCharactersAfterAt: 5,
     maskAtTheRate: false
 };
  
-const email = "my.test.email@testEmail.com";
- 
-const maskedEmail = MaskData.maskEmail2(email, emailMask2Options);
- 
-//Output: my.********@**********om
 
 // exportation de la fonction qui va enregistrer un nouvel utilisateur
 exports.signup =(req,res,next) => {
+const maskedEmail = MaskData.maskEmail2(req.body.email, emailMask2Options);
 bcrypt.hash(req.body.password, 10)
 .then(hash =>{ 
 	const user = new User({
-		email: req.body.email,
+		email: maskedEmail,
 		password: hash
 	});
 	user.save()
@@ -39,7 +35,8 @@ bcrypt.hash(req.body.password, 10)
 
 // exportation de la fonction qui va connecter un utilisateur déjà enregistré
 exports.login = (req, res, next) =>{
- User.findOne({ email: req.body.email })
+const maskedEmail = MaskData.maskEmail2(req.body.email, emailMask2Options);
+ User.findOne({email: maskedEmail })
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé !' });
